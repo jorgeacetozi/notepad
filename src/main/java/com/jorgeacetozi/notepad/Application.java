4339ed6edff6f5fad04755a4cacf617c3b2927f0
Jorge Acetozi <jorge.acetozi@gmail.com>
2017-08-30 23:04:14 -0300

    Add maven release plugin configurations

--- src/main/java/com/jorgeacetozi/notepad/Application.java
+++ src/main/java/com/jorgeacetozi/notepad/Application.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class NotepadApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(NotepadApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 }
