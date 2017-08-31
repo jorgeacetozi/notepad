2896468836bac4863bf8a7b06d1dab2557b3f812
Jorge Acetozi <jorge.acetozi@gmail.com>
2017-08-30 22:55:50 -0300

    Create notepad Spring Boot sample application

---
+++ src/main/java/com/jorgeacetozi/notepad/NotepadApplication.java
@@ -0,0 +1,12 @@
+package com.jorgeacetozi.notepad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotepadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotepadApplication.class, args);
+	}
+}
