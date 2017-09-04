ddd8acf3b5df49691048645c993737d048bc125b
Jorge Acetozi <jorge.acetozi@gmail.com>
2017-09-04 19:36:44 -0300

    Acceptance test for creating notes and Maven acceptance-tests profile

---
+++ src/test/java/com/jorgeacetozi/notepad/acceptanceTests/note/pageObject/NewNotePage.java
@@ -0,0 +1,49 @@
+package com.jorgeacetozi.notepad.acceptanceTests.note.pageObject;
+
+import static java.lang.Thread.sleep;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import com.jorgeacetozi.notepad.note.domain.model.Note;
+
+public class NewNotePage {
+
+	@FindBy(id="newNote")
+	private WebElement newNoteModal;
+	
+	@FindBy(id="newNoteTitle")
+	private WebElement title;
+	
+	@FindBy(id="newNoteContent")
+	private WebElement content;
+	
+	@FindBy(id="btnCreateNewNote")
+	private WebElement createNoteButton;
+	
+	private Long sleep = 2000l;
+	
+	private WebDriver driver;
+	
+    public NewNotePage(WebDriver driver) {
+    	this.driver = driver;
+        PageFactory.initElements(driver, this);
+    }
+	
+	public void create(Note newNote) throws InterruptedException {
+		newNoteModal.click();
+		sleep(sleep);
+		
+		title.sendKeys(newNote.getTitle());
+		content.sendKeys(newNote.getContent());
+		createNoteButton.click();
+		sleep(sleep);
+	}
+
+	public String getMessage() {
+		return driver.findElement(By.className("noty_text")).getText();
+	}
+}
