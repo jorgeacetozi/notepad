14886b809115f36f1aaee3e1e575d8d645dc8517
Jorge Acetozi <jorge.acetozi@gmail.com>
2017-09-09 17:34:13 -0300

    Add Logback configuration and change playground, staging and production profiles to get logback-spring.xml from the file system

--- src/main/java/com/jorgeacetozi/notepad/note/api/NoteController.java
+++ src/main/java/com/jorgeacetozi/notepad/note/api/NoteController.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import javax.validation.Valid;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
@@ -19,22 +21,25 @@ import com.jorgeacetozi.notepad.note.domain.service.NoteService;
 
 @Controller
 public class NoteController {
-	
+
 	@Autowired
 	private NoteService noteService;
-	
+
+	private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
 	@RequestMapping("/")
 	public ModelAndView index() {
-    	ModelAndView modelAndView = new ModelAndView("index");
-    	List<Note> notes = noteService.findAll();
-    	modelAndView.addObject("notes", notes);
-    	return modelAndView;
+		ModelAndView modelAndView = new ModelAndView("index");
+		List<Note> notes = noteService.findAll();
+		modelAndView.addObject("notes", notes);
+		return modelAndView;
 	}
 
 	@PostMapping("/notes")
 	@ResponseBody
 	@ResponseStatus(code = HttpStatus.CREATED)
 	public Note create(@RequestBody @Valid Note note) {
+		logger.debug("Create note request: {}", note);
 		return noteService.create(note);
 	}
 }
